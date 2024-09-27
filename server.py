6689821f57c13528d73f75f4374995a910650e6e
Ayaz Kaya <ayazjkaya@yahoo.com>
2024-09-27 09:38:26 +0200

    changed server script to use OOP

--- server.py
+++ server.py
@@ -2,67 +2,73 @@ import socket
 import threading
 import pickle
 
-clients = []
+host_address = "192.168.1.24"
+port = 3333
 
-def initialize_server_socket():
+class Server:
+    def __init__(self, host_address, port):
+        self.clients = []
 
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket = self.initialize_server_socket(host_address, port)
+        print("server_socket initialized")
+        
+        self.accept_loop(self.server_socket)
 
-    host_address = "192.168.1.24"
-    port = 3333
+    def initialize_server_socket(self, host_address, port):
 
-    server_socket.bind((host_address, port))
-    server_socket.listen(5)
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    return server_socket
+        server_socket.bind((host_address, port))
+        server_socket.listen(5)
 
-def ping(client):
-    try:
-        command = {'command': "ping", "parameters": []}
+        return server_socket
+    
+    def accept_loop(self, server_socket):
 
-        client.send(pickle.dumps(command))
-        client.settimeout(1)
-        response = client.recv(1024)
+        while True:
+            client_socket, client_address = server_socket.accept()
+            clients.append(client_socket)
 
-        if not response:
+            threading.Thread(target=send_command, args=()).start()
+
+    def send_command(self):
+        while True:
+            command = input("Enter command: ")
+
+            command_list = command.split(" ")
+            command_dict = {"command": command_list[0], "parameters": command_list[1:]}
+            command = pickle.dumps(command_dict)
+            
+            to_remove = []
+            
+            for client in self.clients:
+                try:
+                    if not self.ping(client):
+                        to_remove.append(client)
+                    else:
+                        client.sendall(command)
+                except (socket.timeout, socket.error):
+                    to_remove.append(client)
+            
+            # Remove clients after processing the command
+            for client in to_remove:
+                self.clients.remove(client)
+                client.close()
+
+    def ping(self, client):
+        try:
+            command = {'command': "ping", "parameters": []}
+
+            client.send(pickle.dumps(command))
+            client.settimeout(1)
+            response = client.recv(1024)
+
+            if not response:
+                return False
+
+            return pickle.loads(response) == "pong"
+
+        except (socket.timeout, socket.error):
             return False
 
-        return pickle.loads(response) == "pong"
-
-    except (socket.timeout, socket.error):
-        return False
-
-def send_command():
-    while True:
-        command = input("Enter command: ")
-
-        command_list = command.split(" ")
-        command_dict = {"command": command_list[0], "parameters": command_list[1:]}
-        command = pickle.dumps(command_dict)
-        
-        to_remove = []
-        
-        for client in clients:
-            try:
-                if not ping(client):
-                    to_remove.append(client)
-                else:
-                    client.sendall(command)
-            except (socket.timeout, socket.error):
-                to_remove.append(client)
-        
-        # Remove clients after processing the command
-        for client in to_remove:
-            clients.remove(client)
-            client.close()
-
-def main():
-    server_socket = initialize_server_socket()
-    print("server_socket initialized.")
-
-    while True:
-        client_socket, client_address = server_socket.accept()
-        clients.append(client_socket)
-
-        threading.Thread(target=send_command, args=()).start()
-main()
+server = Server(host_address, port)
