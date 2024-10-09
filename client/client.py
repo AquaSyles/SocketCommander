691ae177057ea91e517449939679bd237fdfea60
Ayaz Kaya <ayazjkaya@yahoo.com>
2024-10-09 10:38:30 +0200

    Made client.py use classes

--- client/client.py
+++ client/client.py
@@ -4,60 +4,67 @@ import os
 import threading
 import pickle
 
-commands_directory_path = os.path.join(os.path.dirname(__file__), "commands")
 
-def initialize_client_socket():
+class Client:
+    def __init__(self):
+        self.commands_directory_path = os.path.join(os.path.dirname(__file__), "commands")
 
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client_socket, host_address, port = self.initialize_client_socket()
 
-    host_address = "192.168.1.24"
-    port = 3333
+        self.client_socket.connect((host_address, port))
+        print(f"Connected to host at: {host_address}")
 
-    return client_socket, host_address, port
-
-def initiate_running_path(running_path, parameters):
-    with open(os.path.join(commands_directory_path, "geoinit", "config.pkl"), 'wb') as file:
-        config_dict = {'status': 1, 'parameters': parameters}
-        pickle.dump(config_dict, file)
+        self.accept_loop()
 
 
-def run_py_command(command, parameters):
-    command_path = os.path.join(commands_directory_path, command, f"{command}.py") # Gets the path of the command file
-    running_path =  os.path.join(commands_directory_path, command, "running") 
-    python_version = "python" if os.name == "nt" else "python3" # Needs correct type to run a python file depending on OS
-    
-    initiate_running_path(running_path, parameters) # Command is not allowed to run if running if False, which lets us close threads.
 
-   
-    subprocess.run([python_version, command_path])
-    
+    def initialize_client_socket(self):
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-def main():
-    client_socket, host_address, port = initialize_client_socket()
-    print("client_socket initialized.")
+        host_address = "192.168.1.24"
+        port = 3333
 
-    client_socket.connect((host_address, port))
-    print("Connection established.")
-    
-    try:
-        while True:
-            command = client_socket.recv(1024)
-            command = pickle.loads(command)
+        return client_socket, host_address, port
 
-            command, parameters = command['command'], command['parameters']
-            
-            if command == "ping":
-                client_socket.send(pickle.dumps("pong"))
+    def accept_loop(self):
+        try:
+            while True:
+                command = self.client_socket.recv(1024)
+                print("Ready to receive")
+                command = pickle.loads(command)
 
-            elif command == "close":
-                with open(os.path.join(commands_directory_path, "geoinit", "config.pkl"), 'wb') as file:
-                    config_dict = {'status': 0}
-                    pickle.dump(config_dict, file)
-
-            else:
-                threading.Thread(target=run_py_command, args=(command, parameters,)).start()
+                command, parameters = command['command'], command['parameters']
                 
-    except KeyboardInterrupt:
-        print("Shutting down...")
-        exit()
-main()
+                if command == "ping":
+                    self.client_socket.send(pickle.dumps("pong"))
+
+                elif command == "close":
+                    with open(os.path.join(self.commands_directory_path, "geoinit", "config.pkl"), 'wb') as file:
+                        config_dict = {'status': 0}
+                        pickle.dump(config_dict, file)
+
+                else:
+                    threading.Thread(target=self.run_py_command, args=(command, parameters,)).start()
+                    
+        except KeyboardInterrupt:
+            print("Shutting down...")
+            exit()
+
+
+    def run_py_command(self, command, parameters):
+        command_path = os.path.join(self.commands_directory_path, command, f"{command}.py") # Gets the path of the command file
+        python_version = "python" if os.name == "nt" else "python3" # Needs correct type to run a python file depending on OS
+        
+        self.initiate_running_path(command, parameters) # Command is not allowed to run if running if False, which lets us close threads.
+
+        print("Ready to run python process") 
+        print("command_path", command_path)
+        print("parameters", parameters)
+        subprocess.run([python_version, command_path])
+
+    def initiate_running_path(self, command, parameters):
+        with open(os.path.join(self.commands_directory_path, command, "config.pkl"), 'wb') as file:
+            config_dict = {'status': 1, 'parameters': parameters}
+            pickle.dump(config_dict, file)
+
+client = Client()
